932a5a1680cf80c3f5553dadbf0c62cda3b8ece0
rango198 <137562811+rango198@users.noreply.github.com>
2023-11-11 18:55:00 +0000

    Deploying to gh-pages from @ rango198/goit-js-hw-09@6d8748a898b80c7acfa885b5b210b30d1aef1279 🚀

---
+++ 01-color-switcher.df280832.js
@@ -0,0 +1,2 @@
+const t={btnStart:document.querySelector("button[data-start]"),btnStop:document.querySelector("button[data-stop]"),body:document.querySelector("body")};let e;t.btnStart.addEventListener("click",(a=>{e=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.btnStart.disabled=!0})),t.btnStop.addEventListener("click",(a=>{clearInterval(e),t.btnStart.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.df280832.js.map
